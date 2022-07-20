42dc55ec6aea9fc01a89e961f54072732a87c9fd
Juhan <juhan211@naver.com>
2022-07-20 09:34:52 +0900

    일단 도커 컴포즈 파일 환경을 잡아줬습니닷!악!

--- src/main/java/com/example/testeverything/TestEverythingApplication.java
+++ src/main/java/com/example/testeverything/TestEverythingApplication.java
@@ -8,7 +8,6 @@ public class TestEverythingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestEverythingApplication.class, args);
-        System.out.println(" = ");
     }
 
 }
